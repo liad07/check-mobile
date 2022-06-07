f85a906c7694d6102657c01da81460a16a8364ea
liadk07 <91607695+liad07@users.noreply.github.com>
2022-06-07 18:26:30 +0300

    Add files via upload

---
+++ assests/checkmobile.js
@@ -0,0 +1,11 @@
+try {
+    if (navigator.userAgentData.mobile){
+        document.getElementById("button").textContent="mobile";
+    }
+    else {
+        document.getElementById("button").textContent="not mobile";
+    }
+}
+catch (err){
+    document.getElementById("button").textContent="your browser dont support";
+}
