584a2874713969b81c2fcabb6fb4215f542c4aa7
liadk07 <91607695+liad07@users.noreply.github.com>
2022-06-07 17:40:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-if (navigator.userAgentData.mobile){
-    document.getElementById("button").textContent="mobile";
-}
-else {
-    document.getElementById("button").textContent="not mobile";
-}
+    try {
+        if (navigator.userAgentData.mobile){
+            document.getElementById("button").textContent="mobile";
+        }
+        else {
+            document.getElementById("button").textContent="not mobile";
+        }
+    }
+  catch (err){
+      document.getElementById("button").textContent="your browser dont support";
+  }
